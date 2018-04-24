2504be1b4216dde5b5823ab67d21b5660cb5cd8a
unknown <itmemario2012@gmail.com>
2018-04-24 20:41:13 +0200

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import Header from "./Header";
+import Landing from "./Landing";
+import Surveys from "./Surveys";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Surveys} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
---
+++ client/src/components/Surveys.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Surveys =() => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>DashBoard</h1>
+            
+        </div>
+        
+    )
+}
+
+export default Surveys
