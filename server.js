0fca33f04cbc70867b4e98c03ea119721d48d411
unknown <itmemario2012@gmail.com>
2018-04-25 14:18:25 +0200

    Payement + email

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session')
-const passport = require('passport')
-const keys = require('./config/keys')
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-)
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.connect(keys.mongoURI)
-    .then(console.log('connected'))
-    .catch(err => console.log(err))
+mongoose
+  .connect(keys.mongoURI)
+  .then(console.log("connected"))
+  .catch(err => console.log(err));
 
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
-require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000;
 
